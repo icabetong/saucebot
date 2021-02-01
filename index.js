37f4250acfd2107d1bdb346a8f5cee28799648eb
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-02-01 22:37:36 +0800

    Switched to DotEnv

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import { Client, MessageEmbed } from 'discord.js';
-import { TOKEN } from './config.json';
+import { Client } from 'discord.js';
+import fetch from "node-fetch";
 
+require('dotenv').config();
+
+const TOKEN = process.env.TOKEN;
 const client = new Client();
+
+let url = "https://api.jsonbin.io/b/6017a79cabdf9c556795cb06/2";
+let data;
+fetch(url)
+	.then(data => data.json())
+	.then(output => data = output)
+	.catch(err => data = err);
 
 client.login(TOKEN);
 
@@ -20,5 +30,37 @@ client.on('guildMemberRemove', member => {
 client.on('message', message => {
 	if (message.author.bot) return;
 
-	//message.reply(`testing`);
+	if (message.channel.type == "dm") {
+		
+		message.channel.messages.fetch({ limit: 2 })
+			.then(messages => {
+				const question = messages.last();
+				const answer = messages.first();
+				
+				if (question.content === data.verification.question) {
+					if (data.verification.answers.indexOf(answer.content) > -1) {
+
+						let guild = client.guilds.cache.get(data.guild.debug);
+						if (guild) {
+							let role = guild.roles.cache.get(data.role.debug);
+							if (role) {
+								guild.members.cache.get(message.author.id).roles.add(role);
+								message.author.send("Congratulations! You are now verified.");
+							}
+						}
+					}	
+				}
+			}).catch(error => {
+				console.error(error);
+			})
+	}
+               
+	if (message.content.startsWith("!verify")) {
+
+		if (message.content !== "!verify") {
+			message.reply("There should be no more text after verify");
+		} else {
+			message.author.send(data.verification.question);
+		}
+	}
 })
