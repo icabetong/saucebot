93629107d1328f3a5d0d5f4e4629ff7f3299ece6
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-02-02 14:11:05 +0800

    Finished Verification Polishing

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ import fetch from "node-fetch";
 require('dotenv').config();
 
 const TOKEN = process.env.TOKEN;
+const DEBUG = process.env.debug;
 const client = new Client();
 
 let url = "https://api.jsonbin.io/b/6017a79cabdf9c556795cb06/2";
@@ -20,7 +21,7 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-	member.guild.systemChannel.send(`Welcome **${member.user.tag}**. The Fuhrer is expecting you.`)
+	member.guild.systemChannel.send(`Welcome **${member.user.tag}**. The Führer is expecting you.`)
 })
 
 client.on('guildMemberRemove', member => {
@@ -30,7 +31,10 @@ client.on('guildMemberRemove', member => {
 client.on('message', message => {
 	if (message.author.bot) return;
 
+	
 	if (message.channel.type == "dm") {
+		let guildId = DEBUG ? data.guild.debug : data.guild.production;
+		let roleId = DEBUG ? data.role.debug : data.role.production;
 		
 		message.channel.messages.fetch({ limit: 2 })
 			.then(messages => {
@@ -38,29 +42,33 @@ client.on('message', message => {
 				const answer = messages.first();
 				
 				if (question.content === data.verification.question) {
-					if (data.verification.answers.indexOf(answer.content) > -1) {
+					if (data.verification.answers.includes(answer.content)) {
 
-						let guild = client.guilds.cache.get(data.guild.debug);
-						if (guild) {
-							let role = guild.roles.cache.get(data.role.debug);
-							if (role) {
-								guild.members.cache.get(message.author.id).roles.add(role);
-								message.author.send("Congratulations! You are now verified.");
-							}
-						}
-					}	
+						let guild = client.guilds.cache.get(guildId);
+						let role = guild.roles.cache.get(roleId);
+						if (guild && role) {
+							guild.members.cache.get(message.author.id).roles.add(role);
+							message.author.send("Congratulations! You are now verified.");
+						} else { message.author.send("An error has occurred."); }
+					} else {
+						message.author.send("Wrong answer! Try again later.");
+					}
 				}
 			}).catch(error => {
 				console.error(error);
 			})
 	}
-               
+		 
+	// Verification
 	if (message.content.startsWith("!verify")) {
 
 		if (message.content !== "!verify") {
-			message.reply("There should be no more text after verify");
+			message.reply("There should be no more text after verify.");
 		} else {
 			message.author.send(data.verification.question);
 		}
+	} else if (message.content.startsWith("!find")) {
+
+		
 	}
 })
